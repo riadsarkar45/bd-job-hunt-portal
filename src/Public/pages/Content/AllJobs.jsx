b2524e1ec29a59a69e5746812df0c6545c28014d
riadsarkar45 <riadsarkar45@gmail.com>
2023-12-18 13:59:26 +0600

    search function

--- src/Public/pages/Content/AllJobs.jsx
+++ src/Public/pages/Content/AllJobs.jsx
@@ -2,11 +2,10 @@ import React, { useState, useEffect } from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import { LinearProgress, Card, CardContent } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search'; import { LinearProgress, Card, CardContent } from '@mui/material';
 import useAxiosPublic from '../../../components/Hooks/useAxiosPublic';
 import AllJobSearch from './AllJobSearch/AllJobSearch';
-
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
 function AllJobs() {
   const axiosPublic = useAxiosPublic();
   const [jobs, setJobs] = useState([]);
@@ -15,7 +14,9 @@ function AllJobs() {
   const [filtJobs, setFiltJobs] = useState([]);
   const [filtLoading, setFiltLoading] = useState(true);
   const [datas, setData] = useState(false);
-
+  const [inputValue, setInputValue] = useState('');
+  const [search, setSearch] = useState(false)
+  const [searchResult, setSearchResult] = useState([]);
   useEffect(() => {
     axiosPublic.get('/jobs').then((res) => {
       setJobs(res.data);
@@ -29,13 +30,33 @@ function AllJobs() {
     setFiltJobs(filterJobs);
   }, [degree, jobs]);
 
+  const handleChangeSearch = (event) => {
+    const value = event.target.value;
+    setInputValue(value);
+    if (value.length >= 0) {
+      const filter = jobs?.filter((jo) => jo.roleName?.toLowerCase().includes(value))
+      setSearchResult(filter)
+      setSearch(true)
+    } else {
+      setSearch(false)
+    }
+    console.log(filter)
+  };
+
   return (
     <div style={{ display: 'flex' }} className='w-[80%] m-auto font-sans'>
       <CssBaseline />
       <div className='mt-[6.9rem] h-full shadow-md' style={{ position: 'fixed', width: '15rem', zIndex: 1200 }}>
+        <div className='text-center text-blue-200'>
+          <FilterAltIcon />
+          <p>Filter</p>
+        </div>
         <List>
           <ListItem>
-            <input type="text" placeholder="Search" className="input input-bordered w-full max-w-xs" />
+            <input type="text" placeholder="Search"
+              value={inputValue}
+              onChange={handleChangeSearch}
+              className="input input-bordered w-full max-w-xs" />
           </ListItem>
         </List>
         <List>
@@ -78,6 +99,23 @@ function AllJobs() {
                     </CardContent>
                   </Card>
                 ))
+              ) : search ? (
+
+                searchResult.length <= 0 ? (
+                  <div className='text-center mt-[10rem] text-red-300 font-sans text-2xl'>
+                    <SearchIcon sx={{fontSize:"64px", color:"red"}} />
+                    <p>No result found</p>
+                  </div>
+                ) : (
+                  searchResult.map((data) => (
+                    <Card key={data._id} className="mb-4">
+                      <CardContent>
+                        <AllJobSearch data={data} />
+                      </CardContent>
+                    </Card>
+                  ))
+                )
+
               ) : (
                 jobs.map((data) => (
                   <Card key={data._id} className="mb-4">
